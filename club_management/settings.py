8587b3d211adcde8db31de6a748bca48eea32185
akshay41 <akshujoshi41@gmail.com>
2023-04-28 18:27:17 +0100

    static

--- club_management/settings.py
+++ club_management/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'club_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tej',
+        'USER': 'admin',
+        'PASSWORD': 'a4ZUHj6GfAtXUujL',
+        'HOST':'django-app.civwddw9gdry.eu-north-1.rds.amazonaws.com',
+        'PORT':'3306',
     }
 }
 
